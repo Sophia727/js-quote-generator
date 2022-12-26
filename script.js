371180eda867801d626353d88851ad6e7eae6f50
Sophia <sophiatoukram@gmail.com>
2022-12-26 17:20:00 +0100

    ok

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 let quoteOfTheDay = document.querySelector('.quote');
-let btn = document.getElementsByClassName('generate');
+let btn = document.getElementById('generate');
 let allQuotes =[];
 
-function generate(){
+function generate() {
     fetch('https://type.fit/api/quotes')
     .then((response)=> {
         return response.json();
     })
     .then((response)=> {
         allQuotes = response;
+        
         let quote = allQuotes[Math.floor(Math.random() * allQuotes.length)];
         quoteOfTheDay.textContent = quote.text;
+
     }) 
     .catch((err)=> console.log(err));
 }
-btn.addEventListener('click', generate(quoteOfTheDay));
+generate();
+btn.addEventListener('click', generate);
